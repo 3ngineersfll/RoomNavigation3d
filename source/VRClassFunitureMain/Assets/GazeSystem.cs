a84eddba482a02ee0253e7a7fdd05b038c129aea
3ngineersfll <41350205+3ngineersfll@users.noreply.github.com>
2018-07-18 14:20:11 -0400

    FurnitureRoomVR
    Worked on gaze sytsem c#

--- source/VRClassFunitureMain/Assets/GazeSystem.cs
+++ source/VRClassFunitureMain/Assets/GazeSystem.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GazeSystem : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessGaze();
+    }
+
+    void OnDisable()
+    {
+    }
+
+    public void ProcessGaze()
+    {
+
+        Ray raycastRay = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        Debug.DrawRay(raycastRay.origin, raycastRay.direction * 100);
+
+
+        if (Physics.Raycast(raycastRay, out hitInfo))
+        {
+
+            // Do something to the object
+
+        }
+
+    }
 }
