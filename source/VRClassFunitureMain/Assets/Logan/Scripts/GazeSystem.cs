dec2d18455b947cb73565ae5909bd3e353200c6c
3ngineersfll <41350205+3ngineersfll@users.noreply.github.com>
2018-07-18 20:00:54 -0400

    GazableObject
    GazableObject script was added

--- source/VRClassFunitureMain/Assets/Logan/Scripts/GazeSystem.cs
+++ source/VRClassFunitureMain/Assets/Logan/Scripts/GazeSystem.cs
@@ -3,14 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GazeSystem : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessGaze();
+    }
+
+    void OnDisable()
+    {
+    }
+
+    public void ProcessGaze()
+    {
+
+        Ray raycastRay = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        Debug.DrawRay(raycastRay.origin, raycastRay.direction * 100);
+
+
+        if (Physics.Raycast(raycastRay, out hitInfo))
+        {
+
+            // Do something to the object
+
+            // Check if the object is interactable
+
+            // Check if the object is a new object (first time looking)
+
+            // Set the reticle color
+
+        }
+
+    }
+
+    private void SetReticleColor(Color reticleColor)
+    {
+
+    }
+
+    private void CheckForInput(RaycastHit hitInfo)
+    {
+
+        // Check for down
+
+        // Check for hold
+
+        // Check for release
+    }
 }
