d251521c7ed0f114b6d9e5ca09853c862a086c40
3ngineersfll <41350205+3ngineersfll@users.noreply.github.com>
2018-08-08 08:16:10 -0400

    worked on buttons
    buttons script was added.

---
+++ source/VRClassFunitureMain/Assets/GazeableButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetButtonColor(Color buttonColor)
+    {
+        GetComponent<Image>().color = buttonColor;
+
+    }
+
+    public override void OnPress(RaycastHit hitInfo)
+    {
+        base.OnPress(hitInfo);
+
+        SetButtonColor(Color.green);
+    }
+}
