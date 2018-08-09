8df92baa8227683ed980c6e7397b481210ae8bfb
3ngineersfll <41350205+3ngineersfll@users.noreply.github.com>
2018-08-09 18:57:47 -0400

    added
    added

---
+++ source/VRClassFunitureMain/Assets/Logan/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputMode
+{
+    NONE,
+    TELEPORT,
+    WALK
+}
+
+public class Player : MonoBehaviour {
+
+    public static Player instance;
+
+    public InputMode activeMode = InputMode.NONE;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            GameObject.Destroy(instance.gameObject);
+        }
+
+        instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
