8f638a41fa9ad2298105a3a0200095f4fcd70837
3ngineersfll <41350205+3ngineersfll@users.noreply.github.com>
2018-08-16 12:09:00 -0400

    scale up
    scale +

--- source/VRClassFunitureMain/Assets/Logan/Scripts/GazeableObject.cs
+++ source/VRClassFunitureMain/Assets/Logan/Scripts/GazeableObject.cs
@@ -8,7 +8,12 @@ public class GazeableObject : MonoBehaviour
     public bool isTransformable = false;
 
     private int objectLayer;
-    private const int IGNORE_REYCAST_LAYER = 2;
+    private const int IGNORE_RAYCAST_LAYER = 2;
+
+    private Vector3 initialObjectRotation;
+    private Vector3 initialPlayerRotation;
+
+    private Vector3 initialObjectScale;
 
     public virtual void OnGazeEnter(RaycastHit hitInfo)
     {
@@ -34,7 +39,12 @@ public class GazeableObject : MonoBehaviour
 
             objectLayer = gameObject.layer;
             
-            gameObject.layer = IGNORE_REYCAST_LAYER;
+            gameObject.layer = IGNORE_RAYCAST_LAYER;
+
+            initialObjectRotation = transform.rotation.eulerAngles;
+            initialPlayerRotation = Camera.main.transform.eulerAngles;
+
+            initialObjectScale = transform.localScale;
 
         }
 
@@ -108,6 +118,18 @@ public class GazeableObject : MonoBehaviour
 
         // Change the object's orientation (rotation)
 
+        float rotationSpeed = 10.0f;
+
+        Vector3 currentPlayerRotation = Camera.main.transform.rotation.eulerAngles;
+
+        Vector3 currentObjectRotation = transform.rotation.eulerAngles;
+
+        Vector3 rotationDelta = currentPlayerRotation - initialPlayerRotation;
+
+        Vector3 newRotation = new Vector3(currentObjectRotation.x, initialObjectRotation.y + (rotationDelta.y * rotationSpeed), currentObjectRotation.z);
+
+        transform.rotation = Quaternion.Euler(newRotation);
+
     }
 
     public virtual void GazeScale(RaycastHit hitInfo)
@@ -115,8 +137,25 @@ public class GazeableObject : MonoBehaviour
 
         // Make the object bigger/smaller (scale)
 
+        float scaleSpeed = 0.1f;
+
+        float scaleFactor = 1;
+
+        Vector3 currentPlayerRotation = Camera.main.transform.rotation.eulerAngles;
+        Vector3 rotationDelta = currentPlayerRotation - initialPlayerRotation;
+
+        // If looking up
+        if (rotationDelta.x < 0 && rotationDelta.x > -180.0f || rotationDelta.x > 180.0f && rotationDelta.x < 360.0f)
+        {
+
+            // If greater than 180, map it between 0 - 180
+            if (rotationDelta.x > 180.0f)
+            {
+                rotationDelta.x = 360.0f - rotationDelta.x;
+            }
+
+            scaleFactor = 1.0f + Mathf.Abs(rotationDelta.x) * scaleSpeed;
+
+        }
     }
-
 }
-
-
